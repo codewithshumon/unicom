28614503e72681146f7132ca757a3ce20fdd08b3
Shumon Khan <backlink.shumonkhan@gmail.com>
2024-03-16 16:26:52 +0600

    Working on circle carousel

--- src/components/landing/LandingCard.jsx
+++ src/components/landing/LandingCard.jsx
@@ -7,17 +7,17 @@ import UnionCardArrow from "../global/UnionCardArrow";
 
 const LandingCard = ({ title }) => {
   return (
-    <div className="w-[5vw] h-[15vh] bg-[#FFFFFF]  p-[5vh] rounded-[2vh]">
+    <div className="w-[20vw] h-[60vh] bg-[#FFFFFF]  p-[5vh] rounded-[2vh]">
       <div className=" w-full h-full flex flex-col justify-between gap-5">
         <div>
           <BrandLogo color="#4E4EFF" className="w-[3vw]" />
         </div>
         <div>
-          {/* <h2 className="text-[#4E4EFF] text-[5vh] pb-[5vh]">{title}</h2> */}
+          <h2 className="text-[#4E4EFF] text-[5vh] pb-[5vh]">{title}</h2>
         </div>
         <div className="w-full flex justify-end items-start  ">
           <div>
-            {/* <UnionCardArrow color="#4E4EFF" className="w-[3vw] pb-[3vh]" /> */}
+            <UnionCardArrow color="#4E4EFF" className="w-[3vw] pb-[3vh]" />
           </div>
         </div>
       </div>
--- src/pages/sections/CircleCard.jsx
+++ src/pages/sections/CircleCard.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import gsap from "gsap";
 //import { Draggable } from "gsap/Draggable";
 //import { Flip } from "gsap/Flip";
@@ -13,64 +13,46 @@ const CircleCard = () => {
   const circleItem = gsap.utils.toArray(".circle-item");
   let slice = 360 / circleItem.length;
   let curRotation = 0;
-  const circleContainerRef = useRef(null);
+  const circleContainerRef = useRef();
 
-  ///useEffect(() => {
-  circleItem.forEach((item, index) => {
-    gsap.set(item, {
-      rotation: (360 / circleItem.length) * index,
-      transformOrigin: "40px -100px",
+  useEffect(() => {
+    circleItem.forEach((item, index) => {
+      gsap.set(item, {
+        rotation: (360 / circleItem.length) * index,
+        transformOrigin: "170px -720px",
+      });
     });
-  });
-  //}, [curRotation]);
+  }, [circleItem]);
 
-  gsap.to(circleItem, {
-    rotation: "+=150",
-    ease: "none",
-    repeat: -1,
-    duration: 5,
-  });
-
-  // const prevBtn = prevRef.current;
-  // console.log("prevBtn", prevBtn);
-  // prevBtn.addEventListener("click", () => {
-  //   curRotation += slice;
-  //   gsap.to(circleItem, {
-  //     duration: 0.25,
-  //     ease: "power1.inOut",
-  //     rotation: curRotation,
-  //     overwrite: "auto",
-  //     //onComplete: rotationBtnComplete,
-  //   });
-  // });
+  useEffect(() => {
+    gsap.set(circleContainerRef.current, {
+      transformOrigin: "170px -720px",
+    });
+  }, []);
 
   const handlePrev = () => {
     curRotation += slice;
 
-    console.log("curRotation", curRotation);
-    gsap.to(".circle", {
-      duration: 0.25,
-      ease: "power1.inOut",
-      rotation: curRotation,
-      overwrite: "auto",
-      //onComplete: rotationBtnComplete,
-    });
+    rotateCircle();
   };
 
   const handleNext = () => {
     curRotation -= slice;
-    gsap.to(".circle", {
+    rotateCircle();
+  };
+
+  const rotateCircle = () => {
+    gsap.to(circleContainerRef.current, {
       duration: 0.25,
       ease: "power1.inOut",
       rotation: curRotation,
       overwrite: "auto",
-      //onComplete: rotationBtnComplete,
     });
   };
 
   return (
     <div className="w-full h-[130vh] relative overflow-hidden">
-      <div className=" absolute top-[1vh] left-[20vw] px-[5%] py-[5%] h-full flex flex-col justify-between z-10">
+      <div className="px-[5%] py-[5%] h-full flex flex-col justify-between z-10">
         <div>
           <h1 className="text-[#282866] text-[10vh] leading-none font-[600]">
             Choose <br />
@@ -79,9 +61,9 @@ const CircleCard = () => {
         </div>
         <div
           ref={circleContainerRef}
-          className="circle absolute top-[45vh] max-w-[100vw] "
+          className="absolute left-[40vw] top-[280vh] max-w-[100vw] "
         >
-          <div className=" w-full flex flex-row items-center justify-center gap-[2vw] circle-container">
+          <div className="w-full flex flex-row items-center justify-center ">
             <div className="wheel">
               <button className="circle-item">
                 <LandingCard title="Energy Rebates" />
